96e065bb9a26328c3ee0bde70e7d7d720634d0a6
Naledi Mokoena <nm353292@gmail.com>
2025-11-10 20:32:42 +0200

    fix: wrap App in Router to satisfy checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
