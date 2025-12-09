3b5f05dc7365c8d8dd86c0d39169ac938bf6d827
Naledi Mokoena <nm353292@gmail.com>
2025-12-09 19:00:41 +0200

    Rename FormikForm to formikForm.js for checker compatibility

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/formikForm";
 
+
 function App() {
   return (
     <div>
